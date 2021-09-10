9b108858060d60e483bdc9e73b277b562ed14111
romanpovzyk <roman.povzyk@gmail.com>
2021-09-10 21:24:55 +0300

    Initial commit

---
+++ lecture_1_from_23/from_lecture_1.sql
@@ -0,0 +1,17 @@
+--select * from m_phones
+--select distinct battery_type from m_phones
+--select distinct body_type from m_phones
+--select distinct battery_type, body_type from m_phones
+--select brand_name, phone_name, memory, os from m_phones
+--select * from clients
+
+--select count(*) from m_phones
+
+/*SELECT COUNT(brand_name), 
+		COUNT(DISTINCT body_type) 
+FROM m_phones*/
+
+SELECT MIN(price),
+		MAX(price),
+		AVG(price)
+FROM m_phones
---
+++ lecture_2_from_23/from_lecture_2.sql
@@ -0,0 +1,32 @@
+/*SELECT MIN(price),
+		MAX(price),
+		AVG(price),
+		SUM(price),
+		COUNT(price)
+FROM m_phones*/
+
+/*SELECT COUNT(phone_name) AS Кількість_товарів,
+		SUM(price) AS Сума_цін,
+		AVG(price) AS Середня_ціна,
+		COUNT(price) AS Кількість_телефонів_з_ціною,
+		SUM(price)/COUNT(phone_name),
+		SUM(price)/COUNT(price)
+FROM m_phones*/
+
+/*SELECT brand_name,
+		phone_name,
+		heigh * width * thickness AS volume
+FROM m_phones*/
+
+/*SELECT *
+FROM m_phones
+ORDER BY brand_name DESC,
+		memory DESC*/
+
+SELECT brand_name,
+		phone_name,
+		weight,
+		price,
+		ROUND(price/weight, 2) AS price_gr
+FROM m_phones
+ORDER BY price_gr DESC
